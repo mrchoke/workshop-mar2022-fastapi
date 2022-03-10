d115cda3a55689ae3462cae7b6eecfeb674baa93
Supphachoke Suntiwichaya <mrchoke@gmail.com>
2022-03-10 15:16:30 +0700

    init workshop

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+source  $HOME/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+omz plugin enable zsh-autosuggestions
+omz plugin enable zsh-syntax-highlighting
+
+python -m pip install -U autopep8
