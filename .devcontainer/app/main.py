2a7211857947aacfbfd049a6cab227d7acc278f6
Supphachoke Suntiwichaya <mrchoke@gmail.com>
2022-03-10 16:10:14 +0700

    fixed devcontainer script

--- .devcontainer/app/main.py
+++ .devcontainer/app/main.py
@@ -4,28 +4,10 @@ from fastapi import FastAPI
 
 version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
-app = FastAPI(
-    title="Workshop FastAPI",
-    description="API สำหรับ Workshop",
-    version="1.0",
-    root_path=os.getenv("ROOT_PATH", "")
-)
+app = FastAPI()
 
 
 @app.get("/")
 async def read_root():
     message = f"Hello world! From FastAPI . Using Python {version}"
     return {"message": message}
-
-if __name__ == "__main__":
-
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-        workers=1,
-        proxy_headers=True,
-        forwarded_allow_ips="*",
-        root_path=os.getenv("ROOT_PATH", ""),
-    )
